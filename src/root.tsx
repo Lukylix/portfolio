5bec9ae57d2351e8a2460c50e8859f46d884b494
Lukylix <dev.garcia.lucas@gmail.com>
2023-06-21 18:00:15 +0200

    Partytown

--- src/root.tsx
+++ src/root.tsx
@@ -2,6 +2,7 @@ import { component$, useContextProvider, useStyles$, useStore } from "@builder.i
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
 import { GlobalStore, SiteStore } from "./context";
+import { QwikPartytown } from "./components/partytown/partytown";
 
 import globalStyles from "./global.scss?inline";
 
@@ -17,6 +18,13 @@ export default component$(() => {
 			<head>
 				<meta charSet="utf-8" />
 				<link rel="manifest" href="/manifest.json" />
+				<QwikPartytown forward={["dataLayer.push"]} />
+				<script
+					async
+					type="text/partytown"
+					data-domain="iloa.dev"
+					src="https://plausible.iloa.dev/js/script.js"
+				></script>
 				<RouterHead />
 			</head>
 			<body lang="fr">
